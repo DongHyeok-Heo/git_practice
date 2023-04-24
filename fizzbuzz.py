2b0009b3c1566bdca281543a9b40a15fb2cb4dfc
Donghyeok Heo <missko30@naver.com>
2023-04-24 21:29:33 +0900

    feat: Python env test
    Create do_fizzbuzz and test.

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+def do_fizzbuzz():
+    """
+    fizzbuzz: print fizz buzz fizzbuzz
+    multiple 3: fizz
+    multiple 5: buzz
+    multiple 15: fizzbuzz
+    etc: num
+    """
+    print('hello')
+
+if __name__=='__main__':
+    do_fizzbuzz()
